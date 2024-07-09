2388a3a655cb8ea35844393f1246db44e2815d27
SubaniMalla <subanimalla1@gmail.com>
2024-07-09 15:13:25 +0545

    Solve the exercises  till 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(numList=[],...removeNum) {
+    let newList = [];
+    for(let i=0; i<numList.length;i++){
+        newList.push(numList[i]);
+        for(let j = 0; j<removeNum.length;j++){
+            if(numList[i]===removeNum[j]){
+                newList.pop();
+            }
+        }    
+    }
+    return newList;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(numStart, numEnd) {
+    if((typeof numStart==='number')&&(typeof numEnd==='number')){
+        let sum=0;
+        if(numStart>numEnd){
+            let temp=numEnd;
+            numEnd=numStart;
+            numStart=temp;
+        }
+        if((numEnd<0)||(numStart<0)){
+            return 'ERROR'
+        }
+        for(let i = numStart;i<=numEnd;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+    else{
+        return 'ERROR';
+    }
+    
 };
 
 // Do not edit below this line
