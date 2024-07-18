4381c3ed1b893d2443ba3df376faccef0db5aef9
SubaniMalla <subanimalla1@gmail.com>
2024-07-18 21:21:56 +0545

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total,curr)=>total+curr,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((x,y)=> x*y);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let factorial =1 ;
+  if(x===0){
+    factorial = 1;
+  }
+  else{
+    for(let i =1; i<=x; i++){
+      factorial*=i;
+    }
+  }
+	return factorial;
 };
 
 // Do not edit below this line
