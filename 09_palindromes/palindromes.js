a50967c9e36a2686729f30d68e2e02b4446ae132
SubaniMalla <subanimalla1@gmail.com>
2024-07-18 21:54:56 +0545

    solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (arr) {
+    let palin='';
+    arr=arr.toLowerCase()
+            .replace(/ /g,"")
+            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+            console.log(arr);
+    for(let i =arr.length-1; i >= 0 ; i --){
+            palin+= arr[i];
+    }
+   
+    if(arr==palin){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
