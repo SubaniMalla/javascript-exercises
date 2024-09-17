86721c70513a2163ae7e50f1bfb7801b2ddeaff6
SubaniMalla <subanimalla1@gmail.com>
2024-09-17 23:23:04 +0545

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(z) {
+    let x=0,y=1,ans;
+    if(z<0){
+        return "OOPS";
+    }
+    if(z==0){
+        return 0;
+    }
+    else if(z==1){
+        return 1;
+    }
+    else{
+        for(let i=1;i<z;i++){
+            ans=x+y;
+            x=y;
+            y=ans;
+        }
+        return ans;
+    }
+    
 };
 
 // Do not edit below this line
